6641c42763304601a3752b53eb2250211adef1f0
Adriana <adriana.calvomatos@gmail.com>
2021-11-07 01:10:20 +0000

    calculating revenue based on conditions

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -8,3 +8,14 @@ puts "Exercise 5"
 puts "----------"
 
 # Your code goes here ...
+
+@total_revenue = Store.sum(:annual_revenue)
+puts "Total annual revenue is #{@total_revenue}"
+
+@average_revenue = Store.average(:annual_revenue)
+puts "Average annual revenue is #{@average_revenue}"
+
+@over_million_revenue = Store.where("annual_revenue >= 1000000").count
+puts "#{@over_million_revenue} stores make over 1M in revenue"
+
+
