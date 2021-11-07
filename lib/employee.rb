d31a51d291aad463cab2ea9859be216b30a2a284
Adriana <adriana.calvomatos@gmail.com>
2021-11-07 01:34:16 +0000

    add validations

--- lib/employee.rb
+++ lib/employee.rb
@@ -1,3 +1,11 @@
 class Employee < ActiveRecord::Base
   belongs_to :store
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :hourly_rate, numericality: {
+    only_integer: true,
+    greater_than_or_equal_to: 40,
+    less_than: 200
+  }
+  validates_associated :store
 end
